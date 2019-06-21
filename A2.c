569b3d4451040d5a02ffc38091456cae1cc6d32c
pkharat92 <pkharat92@gmail.com>
2019-06-21 09:08:04 -0700

    Just fixed a typo

--- A2.c
+++ A2.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
 	int execFlag = 0;
 	
 	while(1) {
-		printf("cssc0884% ");
+		printf("cssc0844% ");
 		if(getInput(inputStr) continue;
 		
 		// Exec flag returns 1 if normal command,
